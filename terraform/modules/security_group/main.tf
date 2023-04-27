ad58c83c6c407a36e3cee5237cae84decfa9a489
scofe97 <tscofet@gmail.com>
2023-04-27 12:15:33 +0900

    Feat Outbound 전체 허용

--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -31,19 +31,7 @@ resource "aws_security_group_rule" "kafka_inbound" {
   cidr_blocks = ["10.0.1.0/24"]
 }
 
-# kafka 인스턴스에 대한 아웃바운드 규칙
-# 9092에 대해서 tcp 트래픽 허용한다.
-resource "aws_security_group_rule" "kafka_outbound" {
-  security_group_id = aws_security_group.this.id
-
-  type        = "egress"
-  from_port   = 9092
-  to_port     = 9092
-  protocol    = "tcp"
-  cidr_blocks = ["10.0.1.0/24"]
-}
-
-# Inbound rules for Docker API (Portainer)
+# 도커 API (Portainer)
 # Allow tcp traffic for 2375.
 resource "aws_security_group_rule" "docker_api_inbound" {
   security_group_id = aws_security_group.this.id
@@ -55,17 +43,17 @@ resource "aws_security_group_rule" "docker_api_inbound" {
   cidr_blocks = ["0.0.0.0/0"] # Adjust this to restrict access if needed
 }
 
-# Outbound rules for Docker API (Portainer)
-# Allow tcp traffic for 2375.
-resource "aws_security_group_rule" "docker_api_outbound" {
+# Allow all outbound traffic
+resource "aws_security_group_rule" "all_outbound" {
   security_group_id = aws_security_group.this.id
 
   type        = "egress"
-  from_port   = 2375
-  to_port     = 2375
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"] # Adjust this to restrict access if needed
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 }
+
 
 output "security_group_id" {
   value = aws_security_group.this.id
