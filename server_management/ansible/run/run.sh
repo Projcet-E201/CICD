c695773c6e715fe0e71c6af57a2ad50244c49b92
tscofet <tscofet@gmail.com>
2023-04-30 03:54:52 +0900

    Feat dashboard 수정

--- server_management/ansible/run/run.sh
+++ server_management/ansible/run/run.sh
@@ -1,8 +1,13 @@
 # 자바 & 도커 설치
 ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/java\&docker.yaml
 
+# 파이썬 설치
+ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/python.yaml
+
 # 도커 정상설치 되었는지 확인
 ansible all -i ./inventory/hosts.yaml -m command -a "docker --version" -b
+# 도커 컨테이너 확인
+ansible all -i ./inventory/hosts.yaml -m command -a "docker ps -a" -b
 
 # 도커 네트워크 설치
 ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/docker_init.yaml
