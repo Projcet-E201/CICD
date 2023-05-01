0950e9b9591ae23b705b996c383a0fcd4c864fe9
scofe97 <tscofet@gmail.com>
2023-05-01 10:56:22 +0900

    Feat develop, deployment 서버 인벤토리 추가

--- server_management/ansible/run/run.sh
+++ server_management/ansible/run/run.sh
@@ -1,27 +1,34 @@
 ### SSH 관련 
 scp semsekey.pem semsekey.pem ubuntu@43.201.141.125 /ansible/etc/semsekey.pem
-
+chmod 600 semsekey.pem
 
 ### Ansible Ad hoc
+# 핑 확인
 ansible all -i ./inventory/hosts.yaml -m ping
+ansible all -i ./inventory/hosts2.yaml -m ping
 
 
-### 전체 서버 배포
+### 전체 서버
+# 서버 public ip 확인
+curl -s http://169.254.169.254/latest/meta-data/public-ipv4
+# 서버 private ip 확인
+curl -s http://169.254.169.254/latest/meta-data/local-ipv4
+
 # 자바 & 도커 설치
 ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/java\&docker.yaml
-
-# 파이썬 설치
-ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/python.yaml
-
 # 도커 정상설치 되었는지 확인
 ansible all -i ./inventory/hosts.yaml -m command -a "docker --version" -b
 # 도커 컨테이너 확인
-ansible all -i ./inventory/hosts.yaml -m command -a "docker ps -a" -b
+ansible all -i ./inventory/hosts.yaml -m command -a "docker ps" -b
+
+
+# 파이썬 설치
+ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/python.yaml
 
 # 도커 네트워크 설치
 ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/docker_init.yaml
 
 
-
+### 데이터 서버
 # 데이터 생성서버 배포
 ansible-playbook -i ./inventory/hosts.yaml ./playbook/data_server/datagenerator.yaml
