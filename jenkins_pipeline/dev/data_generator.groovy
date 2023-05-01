a3d67e6366e1e2126234811722c487f396574f36
scofe97 <tscofet@gmail.com>
2023-05-01 11:49:33 +0900

    Feat docker sudo없이 사용하도록 설정

--- jenkins_pipeline/dev/data_generator.groovy
+++ jenkins_pipeline/dev/data_generator.groovy
@@ -2,29 +2,61 @@ pipeline {
     agent any
 
     environment {
-        DOCKER_REGISTRY = '<your-docker-registry-url>'
-        DOCKER_CREDENTIALS_ID = '<your-docker-credentials-id>'
-        APP_NAME = '<your-app-name>'
-        APP_VERSION = '<your-app-version>'
+        DOCKER_HUB_CREDENTIAL = "docker_hub"
+        DOCKER_IMAGE = 'scofe/data_generator'
+        DOCKER_TAG = 'latest'
+
+        credentialsGithubId = "github"
+        repo = "https://github.com/Projcet-E201/DataGenerator.git"
+        branch = 'main'
+
+        ANSIBLE_INVENTORY_PATH = "/home/ubuntu/CICD/server_management/ansible/inventory"
+        ANSIBLE_PLAYBOOK_PATH = "/home/ubuntu/CICD/server_management/ansible/playbook"
     }
 
     stages {
+        stage("Git clone") {
+            steps {
+                 git branch: "${env.branch}", url: "${env.repo}", credentialsId: "${env.credentialsGithubId}"
+            }
+        }
+
+        // stage('Add application.yml') {
+        //     steps {
+        //         sh "pwd"
+        //         sh "cp /home/ubuntu/secret/data_generator/ ./src/main/resources"
+        //     }
+        // }
+
+        stage("Gradle build") {
+            steps {
+                sh "chmod +x gradlew"
+                sh "./gradlew clean build"
+            }
+        }
+
         stage('Build Docker Image') {
             steps {
+                sh 'sudo usermod -aG docker jenkins' // add jenkins user to docker group
                 script {
-                    // Retrieve source code from your repository
-                    checkout scm
+                    docker.build("${DOCKER_IMAGE}:${DOCKER_TAG}")
+                }
+            }
+        }
 
-                    // Build Docker image
-                    dockerImage = docker.build("${APP_NAME}:${APP_VERSION}")
-
-                    // Log in to the Docker registry
-                    docker.withRegistry("${DOCKER_REGISTRY}", "${DOCKER_CREDENTIALS_ID}") {
-                        // Push the Docker image to the registry
-                        dockerImage.push("${APP_VERSION}")
-                        dockerImage.push('latest')
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    docker.withRegistry('https://registry.hub.docker.com', "${env.DOCKER_HUB_CREDENTIAL}") {
+                        docker.image("${DOCKER_IMAGE}:${DOCKER_TAG}").push()
                     }
                 }
+            }
+        }
+
+        stage('Deploy with Ansible') {
+            steps {
+                sh "ansible-playbook -i ${env.ANSIBLE_INVENTORY_PATH}/hosts.yaml ${env.ANSIBLE_PLAYBOOK_PATH}/data_server/datagenerator.yaml"
             }
         }
     }
