fab328f4b6d1e3363421bcf3ecfc9150c68d0cfd
scofe97 <tscofet@gmail.com>
2023-05-01 17:57:16 +0900

    Feat 마무리

---
+++ jenkins_pipeline/dev/data_division.groovy
@@ -0,0 +1,72 @@
+pipeline {
+    agent any
+
+    environment {
+        DOCKER_HUB_CREDENTIAL = "docker_hub"
+        DOCKER_IMAGE = 'scofe/data_division'
+        DOCKER_TAG = 'latest'
+
+        credentialsGithubId = "github"
+        repo = "https://github.com/Projcet-E201/DataDivision"
+        branch = 'main'
+
+        ANSIBLE_INVENTORY_PATH = "/home/ubuntu/CICD/server_management/ansible/inventory"
+        ANSIBLE_PLAYBOOK_PATH = "/home/ubuntu/CICD/server_management/ansible/playbook"
+    }
+
+    stages {
+        stage("Git clone") {
+            steps {
+                 git branch: "${env.branch}", url: "${env.repo}", credentialsId: "${env.credentialsGithubId}"
+            }
+        }
+
+        stage('Add application.yml') {
+            steps {
+                sh "pwd"
+                sh "cp /home/ubuntu/secret/data_division/application-sercret.yaml ./src/main/resources"
+            }
+        }
+
+        stage("Gradle build") {
+            steps {
+                sh "chmod +x gradlew"
+                sh "./gradlew clean build"
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    docker.build("${DOCKER_IMAGE}:${DOCKER_TAG}")
+                }
+            }
+        }
+
+        stage('Push Docker Image') {
+            steps {
+                script {
+                    docker.withRegistry('https://registry.hub.docker.com', "${env.DOCKER_HUB_CREDENTIAL}") {
+                        docker.image("${DOCKER_IMAGE}:${DOCKER_TAG}").push()
+                    }
+                }
+            }
+        }
+
+        // stage('Deploy with Ansible') {
+        //     steps {
+        //         sh 'sudo chmod 644 /home/ubuntu/CICD/server_management/ansible/playbook/data_server/datagenerator.yaml'
+            
+        //         sh "cat ${env.ANSIBLE_PLAYBOOK_PATH}/data_server/datagenerator.yaml"
+        //         sh "ansible-playbook -i ${env.ANSIBLE_INVENTORY_PATH}/hosts.yaml ${env.ANSIBLE_PLAYBOOK_PATH}/data_server/datagenerator.yaml"
+        //     }
+        // }
+    }
+
+    post {
+        always {
+            // Clean up workspace
+            cleanWs()
+        }
+    }
+}
