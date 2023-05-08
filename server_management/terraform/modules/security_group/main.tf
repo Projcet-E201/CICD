2221f2fd4ce885b50394ffb3ecf8f800c1ae061e
scofe97 <tscofet@gmail.com>
2023-05-08 11:30:38 +0900

    Feat Portainer sg 9000 추가

--- server_management/terraform/modules/security_group/main.tf
+++ server_management/terraform/modules/security_group/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group_rule" "portainer_inbound" {
   security_group_id = aws_security_group.this.id
 
   type        = "ingress"
-  from_port   = 8000
+  from_port   = 9000
   to_port     = 9000
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
