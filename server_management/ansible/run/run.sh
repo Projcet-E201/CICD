81da28af5424af19d2063878fc3c3ae940818cf1
tscofet <tscofet@gmail.com>
2023-05-09 23:33:30 +0900

    Feat Ansible 수정

--- server_management/ansible/run/run.sh
+++ server_management/ansible/run/run.sh
@@ -3,8 +3,6 @@ scp semsekey.pem semsekey.pem ubuntu@43.201.141.125 /ansible/etc/semsekey.pem
 chmod 600 semsekey.pem
 sudo chmod 777 /etc/ansible
 
-scp semsekey.pem semsekey.pem ubuntu@52.78.197.47 /ansible/etc/semsekey.pem
-
 ### Ansible Ad hoc
 # 핑 확인
 ansible all -i ./inventory/hosts.yaml -m ping
@@ -17,19 +15,12 @@ curl -s http://169.254.169.254/latest/meta-data/public-ipv4
 # 서버 private ip 확인
 curl -s http://169.254.169.254/latest/meta-data/local-ipv4
 
-# 자바 & 도커 설치
+# 자바 & 도커 & 환경설정
 ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/init.yaml
 # 도커 정상설치 되었는지 확인
 ansible all -i ./inventory/hosts.yaml -m command -a "docker --version" -b
 # 도커 컨테이너 확인
 ansible all -i ./inventory/hosts.yaml -m command -a "docker ps" -b
-
-
-# 파이썬 설치
-ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/python.yaml
-
-# 도커 추가설정
-ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/docker_init.yaml
 
 
 ### 데이터 서버
