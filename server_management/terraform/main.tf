0546401191c80939db78cc9dcedeb8de08cf15fe
scofe97 <tscofet@gmail.com>
2023-05-12 11:07:42 +0900

    Feat prometheus 설정변경

--- server_management/terraform/main.tf
+++ server_management/terraform/main.tf
@@ -113,14 +113,6 @@ resource "aws_eip" "jenkins" {
   }
 }
 
-# Elastic IP for DataDivision instance
-resource "aws_eip" "DataDivision" {
-  instance = aws_instance.DataDivision.id
-
-  tags ={
-    Name = "data_division-instance-eip"
-  }
-}
 
 output "jenkins_instance_public_ip" {
   value = aws_instance.jenkins.public_ip
