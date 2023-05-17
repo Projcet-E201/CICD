4da067b478a3c9303398bcfccef603ff59d66d33
scofe97 <tscofet@gmail.com>
2023-05-17 15:05:50 +0900

    Feat update

--- server_management/ansible/run/run.sh
+++ server_management/ansible/run/run.sh
@@ -1,12 +1,12 @@
 ### SSH 관련 
-sudo scp semsekey2.pem semsekey2.pem ubuntu@13.209.213.175:/etc/ansible/semsekey2.pem
+scp -i semsekey2.pem K8E201T.pem ubuntu@13.209.213.175:/etc/ansible/K8E201T.pem
+scp -i semsekey2.pem semsekey2.pem ubuntu@13.209.213.175:/etc/ansible/semsekey2.pem
 chmod 600 semsekey2.pem
 sudo chmod 777 /etc/ansible
 
 ### Ansible Ad hoc
 # 핑 확인
 ansible all -i ./inventory/hosts.yaml -m ping
-ansible all -i ./inventory/hosts2.yaml -m ping
 
 
 ### 전체 서버
@@ -17,11 +17,9 @@ curl -s http://169.254.169.254/latest/meta-data/local-ipv4
 
 # 자바 & 도커 & 환경설정
 ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/init.yaml
-ansible-playbook -i ./inventory/hosts2.yaml ./playbook/all/init.yaml
 
 # cadvisor 추가
 ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/cadvisor_playbook.yaml
-ansible-playbook -i ./inventory/hosts2.yaml ./playbook/all/cadvisor_playbook.yaml
 
 # 한국시간 변경
 ansible-playbook -i ./inventory/hosts.yaml ./playbook/all/time_playbook.yaml
